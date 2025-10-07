e64858e6d16415aeee4f39d5ce9d1502c7af95d6
lifeisgenie <walker3z@naver.com>
2025-10-07 19:32:39 +0900

    Initial commit to backend branch

---
+++ src/main/java/DumbAndDumber/Danchive/DanchiveApplication.java
@@ -0,0 +1,13 @@
+package DumbAndDumber.Danchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DumbAndDumber/Danchive/DanchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package DumbAndDumber.Danchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
